2a037999d1781d8ad7808c129fbfdf73bb4c97b3
M.Saleh <mega_unknown@hotmail.com>
2020-07-17 21:32:31 -0500

    Clean Code

--- 2D Array DS/2D Array - DS CPP.cpp
+++ 2D Array DS/2D Array - DS CPP.cpp
@@ -1,6 +1,3 @@
-// 2D Array - DS CPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 #include <vector>
@@ -13,22 +10,6 @@ class OldSol;
 
 class NewSol {
 public:
-	//Calculate HourGlassSum For Multi-Dim Array
-	/*int GetHourGlassSumValue(int** ss, int iStartCol, int iStartRow)
-	{
-		int iSum = 0;
-
-		if (iStartCol + 3 > 6 || iStartRow + 3 > 6)
-			return 0;
-
-		iSum = ss[iStartRow][iStartCol] + ss[iStartRow][iStartCol + 1] + ss[iStartRow][iStartCol + 2];
-		iSum += ss[iStartRow + 1][iStartCol + 1];
-		iSum += ss[iStartRow + 2][iStartCol] + ss[iStartRow + 2][iStartCol + 1] + ss[iStartRow + 2][iStartCol + 2];
-
-		return iSum;
-	}*/
-
-	//Calculate HourGlassSum For Vec of Vec
 	int GetHourGlassSumValue(vector<vector<int>> ss, int iStartCol, int iStartRow)
 	{
 		int iSum = 0;
@@ -88,101 +69,3 @@ int main()
 
 	system("pause");
 }
-/*
-Test Case 1
-{-1 ,-1, 0 ,-9 ,-2 ,-2}
-{-2 ,-1 ,-6 ,-8 ,-2 ,-5}
-{-1 ,-1 ,-1 ,-2 ,-3 ,-4}
-{-1 ,-9 ,-2 ,-4, -4 ,-5}
-{-7 ,-3 ,-3 ,-2, -9 ,-9}
-{-1 ,-3 ,-1, -2, -4, -5}
-
-// -6
-Test Case 2
-{0 ,-4, -6 ,0, -7 ,-6},
-{-1, -2 ,-6, -8 ,-3, -1},
-{-8 ,-4, -2, -8, -8 ,-6},
-{-3, -1 ,-2 ,-5, -7 ,-4},
-{-3 ,-5, -3, -6, -6 ,-6},
-{-3, -6,, 0, -8 ,-6, -7}
-
-
-//-19
-
-
-*/
-
-class OldSol {
-public:
-
-	// Complete the hourglassSum function below.
-	int hourglassSum(vector<vector<int>> vec)
-	{
-		int cap = vec.size();
-		//Initialize New Matrix
-		int** matrix = new int*[cap];
-		for (int c = 0; c < cap; ++c)
-			matrix[c] = new int[cap];
-
-		//Convert Vector of Vectors to MultiDimention Array.
-		int iCounter = 0;
-		for (vector<vector<int>>::iterator it = vec.begin(); it != vec.end(); ++it)
-		{
-			vector<int> temp = *it;
-			int size = temp.capacity();
-			for (int j = 0; j < size; j++)
-			{
-				matrix[iCounter][j] = temp.at(j);
-				cout << setw(2) << matrix[iCounter][j] << " ";
-			}
-			iCounter++;
-			cout << "\n";
-		}
-
-		return 0;
-	}
-
-	int GetHourGlassSumValue(int** ss, int iStartCol, int iStartRow, int dim)
-	{
-		int iSum = 0;
-
-		if (iStartCol + 3 > dim || iStartRow + 3 > dim)
-			return 0;
-
-		iSum = ss[iStartRow][iStartCol] + ss[iStartRow][iStartCol + 1] + ss[iStartRow][iStartCol + 2];
-		iSum += ss[iStartRow + 1][iStartCol + 1];
-		iSum += ss[iStartRow + 2][iStartCol] + ss[iStartRow + 2][iStartCol + 1] + ss[iStartRow + 2][iStartCol + 2];
-
-		return iSum;
-	}
-
-	int** SetValues(int** arr, int dim)
-	{
-		//**********************************
-		int** matrix = new int*[dim];
-		for (int c = 0; c < dim; ++c)
-			matrix[c] = new int[dim];
-		//**********************************
-		for (int i = 0; i < dim; i++)
-			for (int j = 0; j < dim; j++)
-				matrix[j][i] = GetHourGlassSumValue(arr, i, j, dim);
-		return matrix;
-	}
-
-	int FindMax(int** matrix, int dim)
-	{
-		int iMax = 0;
-
-		for (int i = 0; i < dim; i++)
-		{
-			for (int j = 0; j < dim; j++)
-			{
-				if (matrix[i][j] > iMax)
-					iMax = matrix[i][j];
-			}
-		}
-		return iMax;
-	}
-
-
-};
